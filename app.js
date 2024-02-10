6b06cfa8324daa70d7c57409162ce9ea929808bb
ViviSuarez22 <vivisuarez22@hotmail.com>
2024-02-09 19:57:36 -0500

    Práctica 1

---
+++ app.js
@@ -0,0 +1,30 @@
+let numeroSecreto = 6;
+let numeroUsuario = prompt("Me indicas un n�mero por favor:");
+
+console.log(numeroUsuario);
+
+if (numeroUsuario == numeroSecreto) {
+    alert('Acertaste el n�mero');
+}
+
+//Desafio Clase 1: Definiendo Variables y if
+alert('¡Bienvenida y bievenido a nuestro sitio web!');
+
+let edad = 25;
+
+let numeroDeVentas = 50;
+
+let saldoDisponible = 1000;
+
+alert('¡Error! Completa todos los campos');
+
+let mensajeDeError = "¡Error! Completa todos los campos";
+alert(mensajeDeError);
+
+let nombreDeUsuario = prompt ("¿Cuál es tu nombre?");
+
+let edadUsuario = prompt ("¿Cuál es tu edad?");
+
+if (edadUsuario >= 18 ) {
+    alert ('¡Puede obtener su licencia de conducir!');
+}
